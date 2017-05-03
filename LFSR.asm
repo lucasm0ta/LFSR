e7228052206ce56912682473b85c5d1b7d8b3132
Lucas Mota <lmotaribeiro@gmail.com>
2017-05-03 03:39:23 -0300

    Fix LFSR in C.

--- LFSR.asm
+++ LFSR.asm
@@ -1,43 +1,42 @@
-; LFSR NOISE SOURCE
-; Uses 11 bytes of memory:
-; SHIFT_REG3, SHIFT_REG2, SHIFT_REG1, SHIFT_REG0
-; NEW_REG0  The replacement SHIFT_REG0 byte
-; TEMP_REG3A    Exactly what they say on the tin.
-; TEMP_REG3B
-; TEMP_REG2A    Temporary copies of REGs 2 &amp;amp;amp;amp; 3
-; TEMP_REG2B
+%macro _output 2		;%1=format,%2=value
+        push	%2
+        push	%1
+        call	printf
+        add	esp,8
+%endmacro
+section .text
+        global 	main
+        extern 	printf
+        LFSR_MASK	equ	0Ch 	;define LFSR MASK for  x^4+x^3+1
+        LFSR_SEED	equ	0Fh 	;define SEED
+main:
+        xor	eax,eax			;eax=0
+        xor	ebx,ebx			;ebx=0
+        mov	eax,LFSR_SEED		;eax=LFSR_SEED
 
-NoiseSource:
-    ; First I need to copy two bytes I'm going to muck with
-    movf    SHIFT_REG3, w
-    movwf   TEMP_REG3A  ; I need 2 copies for left/right shifts
-    movwf   TEMP_REG3B
-    movwf   NEW_REG0    ; Get bit 32 while we've got it
-    movf    SHIFT_REG2, w
-    movwf   TEMP_REG2A  ; Two copies of this too
-    movwf   TEMP_REG2B
 
-    ; Next, I need to XOR all the bytes together
-    rlf TEMP_REG2A, f   ; Left shift 1st set of copies
-    rlf TEMP_REG3A, f
-    rlf TEMP_REG2A, f
-    rlf TEMP_REG3A, w
-    xorwf   NEW_REG0, f ; XOR with bit 30
-    rrf TEMP_REG3B, f   ; Right shift 2nd set of copies
-    rrf TEMP_REG2B, f
-    movf    TEMP_REG2B, w
-    xorwf   NEW_REG0, f ; XOR with bit 25
-    rrf TEMP_REG3B, f
-    rrf TEMP_REG2B, w
-    xorwf   NEW_REG0, f ; XOR with bit 26
+print_loop:
+        call	shift_lfsr		;call to shif_lfsr procedure
+        ;push	eax			;save eax on stack
+        ;_output	format_out,eax		;print number
+        ;pop	eax			;get eax
+        cmp	eax,LFSR_SEED		;compare with seed
+        jne	print_loop		;continue loop
 
-    ; Perform a byte shift on the whole 32-bit register
-    movf SHIFT_REG2, w  ; Put SR2 into SR3
-    movwf SHIFT_REG3
-    movf SHIFT_REG1, w  ; Put SR1 into SR2
-    movwf SHIFT_REG2
-    movf SHIFT_REG0, w  ; Put SR0 into SR1
-    movwf SHIFT_REG1
-    movf NEW_REG0, w        ; Put NEW_REG0 into SR0
-    movwf SHIFT_REG0
-    return
+        mov	al,1			;al=1
+        xor	ebx,ebx			;ebx=0
+        int	80h			;int 80h
+
+shift_lfsr:
+        mov	ebx,eax			;save eax in ebx
+        and	ebx,1			;ebx=ebx AND 1
+        shr	eax,1			;shift right eax with 1
+        cmp	ebx,1			;compare ebx with 1
+        jne	end			;if not equal, end
+        mov	ebx,LFSR_MASK		;if equal, ebx=mask and
+        xor	eax,ebx			; apply mask to eax
+end:
+        ret
+
+section .data
+        format_out	db	"0x%08x",10
--- main.c
+++ main.c
@@ -1,3 +1,45 @@
+#include <stdint.h>
+#include <math.h>
+#include <stdio.h>
+
+uint32_t lfsr_c(uint32_t seed);
+
+// Seed
+uint32_t seed = 0x0C73;
+
 int main() {
-    return 0;
+    int recurrence[4096]; // Bits range;
+
+    int counter;
+    // Set all recurrences to 0
+    for (counter = 0; counter < 4096; counter++) {
+        recurrence[counter] = 0;
+    }
+
+    // Adds randoms to its recurrence range
+    for (counter = 0; counter < 0x1000000; counter++) {
+        uint32_t test = lfsr_c(seed);
+        recurrence[(test & 0x0fff)%4096]++; // Masks first bits to use just 24
+        seed = test;
+    }
+    // Calculate chi-squared
+    double ret = 0;
+    for (counter = 0; counter < 4096; counter++) {
+        printf("%d:%d\n", counter, recurrence[counter]);
+        ret += pow(recurrence[counter] - 4096, 2) / 4096;
+    }
+    printf("X^2_4096 = %lf", ret);
+
+}
+uint32_t lfsr_c(uint32_t seed) {
+    uint32_t start_state = seed; // Bitmask to have just 24bits
+    uint32_t lfsr = start_state;
+    uint32_t bit;                    // Must be 32bit to allow bit << 21 later in the code
+    unsigned period = 0;
+
+    // Taps: 24 22 21 19 16 11 3; Polynomial: x^24 + x^22 + x^21 + x^19 + x^16 + x^11 + x^3 + 1
+    bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ^ (lfsr >> 8) ^ (lfsr >> 13) ^ (lfsr >> 21))& 1;
+    lfsr =  (lfsr >> 1) | (bit << 23);
+
+    return lfsr; // Bitmask to have just 24bits
 }
